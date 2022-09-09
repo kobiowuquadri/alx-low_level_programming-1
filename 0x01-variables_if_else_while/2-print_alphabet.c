3dfa45c0214b1b0136069c0d83ba904c836ff41f
Yttrium <77174957+Miftau@users.noreply.github.com>
2022-09-09 16:52:54 +0100

    Alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - print lowercase and uppercase a-zA-Z using putchar
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lower = 'a';
+	char upper = 'A';
+
+	while (lower <= 'z') /*print lowercases a-z*/
+	{
+		putchar(lower);
+		lower++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
